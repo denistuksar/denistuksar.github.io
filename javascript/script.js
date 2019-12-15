de1347c8d711e060aea4b62f37a8e894ea125e96
denistuksar <dtuksar6@gmail.com>
2019-12-15 21:00:42 +0100

    Smooth Scroll

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    targetPosition -= 83;
+    var startPosition = window.pageYOffset;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime == null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, targetPosition, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed<duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+
+    requestAnimationFrame(animation);
+
+}
+
+
+
+var section1 = document.querySelector('.section-1');
+var section2 = document.querySelector('.section-2');
+var section3 = document.querySelector('.section-3');
+var section0 = document.querySelector('.section-0');
+
+
+
+section1.addEventListener('click', () => {
+    smoothScroll('.section1', 1000);
+})
+
+section2.addEventListener('click', () => {
+    smoothScroll('.section2', 1000);
+})
+
+section3.addEventListener('click', () => {
+    smoothScroll('.section3', 1000);
+})
+
+section0.addEventListener('click', () => {
+    smoothScroll('.section0', 1000);
+})
+
+
