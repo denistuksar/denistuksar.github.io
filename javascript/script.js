6921c11d0c7463a16f25720682e15a5bbdee3855
denistuksar <dtuksar6@gmail.com>
2019-12-17 23:18:52 +0100

    Form Validation

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ function smoothScroll(target, duration) {
         var timeElapsed = currentTime - startTime;
         var run = ease(timeElapsed, startPosition, targetPosition, duration);
         window.scrollTo(0, run);
-        if(timeElapsed<duration) requestAnimationFrame(animation);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
     }
 
     function ease(t, b, c, d) {
@@ -21,17 +21,19 @@ function smoothScroll(target, duration) {
     };
 
     requestAnimationFrame(animation);
-    var element = document.getElementById("collapsibleNavbar");
-    element.classList.remove("show");
+    var element = document.getElementById('collapsibleNavbar');
+    element.classList.remove('show');
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
-function scrollFunction(){
-    if(window.pageYOffset>100){
+function scrollFunction() {
+    if (window.pageYOffset > 100) {
         toTop.style.display = "block";
     } else {
-      toTop.style.display = "none";
+        toTop.style.display = "none";
     }
 };
 
@@ -40,7 +42,13 @@ var section2 = document.querySelector('.section-2');
 var section3 = document.querySelector('.section-3');
 var section0 = document.querySelector('.section-0');
 var toTop = document.querySelector('.toTop');
-
+var email = document.getElementById('email');
+var nameInput = document.getElementById('name');
+var message = document.getElementById('message');
+var submit = document.getElementById('sendMessageButton');
+var emailVerified = false;
+var nameVerified = false;
+var messageVerified = false;
 
 section1.addEventListener('click', () => {
     smoothScroll('.section1', 1000);
@@ -62,6 +70,49 @@ toTop.addEventListener('click', () => {
     smoothScroll('.section0', 1000);
 })
 
+nameInput.addEventListener('blur', () => {
+    if (nameInput.value == '') {
+        nameInput.classList.add('invalid');
+        nameVerified = false;
+    } else {
+        nameInput.classList.remove('invalid');
+        nameVerified = true;
+    }
+});
 
+email.addEventListener('focus', () => {
+    const filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.value == '' || filter.test(email.value) == false) {
+        email.classList.add('invalid');
+        emailVerified = false;
+    } else {
+        email.classList.remove('invalid');
+        emailVerified = true;
+    }
+});
 
+message.addEventListener('blur', () => {
+    if (message.value == '') {
+        message.classList.add('invalid');
+        messageVerified = false;
+    } else {
+        message.classList.remove('invalid');
+        messageVerified = true;
+    }
+});
 
+submit.addEventListener('click', (event) => {
+    if (nameVerified === false || emailVerified === false || messageVerified === false) {
+        event.preventDefault();
+        alert("Invalid entries found, please correct and resubmit");
+    }
+    if(nameVerified===false){
+        nameInput.classList.add('invalid');
+    }
+    if(emailVerified===false){
+        email.classList.add('invalid');
+    }
+    if(messageVerified===false){
+        message.classList.add('invalid');
+    }
+});
